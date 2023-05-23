a5a6367dc350a98c3386f5f72a52b0cc99d5be55
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-05-22 23:24:03 -0300

    Primeiro Commit: Iniciando jogo da velha em swiftUI

---
+++ jogo-da-velha/jogo_da_velhaApp.swift
@@ -0,0 +1,17 @@
+//
+//  jogo_da_velhaApp.swift
+//  jogo-da-velha
+//
+//  Created by cassio on 22/05/23.
+//
+
+import SwiftUI
+
+@main
+struct jogo_da_velhaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
