ca1f065114bfdc8a565cdcfa6c9c73ed85de03af
Cassio Luciano da silva <37276985+Cassiolucianoo@users.noreply.github.com>
2023-05-23 22:07:15 -0300

    Tela de jogo 30% finalizada

--- jogo-da-velha/initView.swift
+++ jogo-da-velha/initView.swift
@@ -7,38 +7,49 @@
 
 import SwiftUI
 
-
-let colunas: [GridItem] = [GridItem(.flexible()),  GridItem(.flexible()), GridItem(.flexible()),]
+var nome =  "vish"
 
 struct ContentView: View {
+    
+    let colunas: [GridItem] = [GridItem(.flexible()),  GridItem(.flexible()), GridItem(.flexible()),]
+
+    @State private var movimentos: [Move?] = Array(repeating: nil, count: 9)
+    @State private var partidaDoJogador = true
+    
     var body: some View {
-        
-        
+       
         /* GeometryReader é um contêiner de visualização que permite acessar informações de layout e geometria da visualização pai. A palavra-chave in é usada para delimitar o escopo da cláusula de fechamento. */
         GeometryReader{ geometry in
-            
+            Color(.yellow).opacity(0.9)
+                .ignoresSafeArea()
             VStack {
                 Spacer()
                 
                 /* LazyVGrid(columns: colunas) {: LazyVGrid é um contêiner de visualização que organiza as visualizações em um layout de grade verticalmente. O parâmetro columns define o número de colunas na grade.*/
-                LazyVGrid(columns: colunas){
+                LazyVGrid(columns: colunas, spacing: 0){
                     
                     /*  ForEach é usado para iterar sobre uma sequência e criar visualizações para cada elemento. Neste caso, estamos criando 9 visualizações, numeradas de 0 a 8, e atribuindo cada valor à variável i. */
                     ForEach(0..<9){ i in
+                        
                         ZStack{
+                          
+                           Circle()
+                                .foregroundColor(.white).opacity(10)
+                                .frame(width: geometry.size.width / 3 - 15,
+                                       height: geometry.size.width / 3 - 15)
                             
-                            Button(action: {
-                                
-                                print(i)
-                                // Ação a ser executada quando o botão for pressionado
-                            }) {
-                                Image("donut1")
+                                Image(movimentos[i]?.indicarJogador ?? "")
                                 // O método resizable() permite que a imagem se ajuste ao tamanho especificado.
                                     .resizable()
                                 // Aqui estamos definindo o tamanho do quadro da imagem como 100x100 pontos.
-                                    .frame(width:100,height: 100)
+                                    .frame(width:140,height: 140)
+                                    .foregroundColor(.white)
                                 
-                            }
+                            }.onTapGesture {
+                                
+                                if isSquareOccupied(in: movimentos, forIndex: i){return}
+                                movimentos[i] = Move(jogador: partidaDoJogador ? .humano: .maquina, quadroIndex: i)
+                                partidaDoJogador.toggle()
                         }
                     }
                 }
@@ -47,12 +58,16 @@ struct ContentView: View {
             }
             //Adiciona preenchimento ao redor do conteúdo para criar um espaço adicional.
             .padding()
+            
         }
+    }
+    func isSquareOccupied(in moves: [Move?], forIndex index: Int) -> Bool {
+        return moves.contains(where: { $0?.quadroIndex == index })
     }
 }
 /* Aqui, estamos definindo uma enumeração chamada Jogador. Essa enumeração possui dois casos: jogador e maquina, que representam os possíveis jogadores em um jogo. */
 enum Jogador{
-    case jogador, maquina
+    case humano, maquina
 }
 
 /* struct Move {: Estamos definindo uma estrutura chamada Move. Essa estrutura possui duas propriedades:
@@ -65,10 +80,10 @@ struct Move {
     
     /* Aqui, estamos declarando uma propriedade computada chamada indicarJogador, que retorna uma string. Essa propriedade não possui um bloco de código explícito, mas usa a sintaxe do operador ternário para retornar diferentes strings com base no caso da enumeração Jogador:
      
-     Se jogador for igual a .maquina, a propriedade retorna a string "donut1".
+     Se jogador for igual a .humano, a propriedade retorna a string "donut1".
      Caso contrário, se jogador for igual a .jogador, a propriedade retorna a string "donut2". */
     var indicarJogador: String{
-        return jogador == .maquina ? "donut1" : "donut2"
+        return jogador == .humano ? "donut1" : "donut2"
         
     }
 }
